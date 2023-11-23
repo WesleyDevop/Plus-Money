e2d6c9510090908f7b6eb299f9ac117b0acf72e7
WellytonNasDev <wesleydevop@gmail.com>
2023-11-23 13:13:39 -0300

    Atualizações e melhorias no Modal

--- public/script/app.js
+++ public/script/app.js
@@ -1,8 +1,25 @@
-let openModal = document.querySelector(".btn_adicionar");
-let modalAdicionar = document.querySelector(".modal_adicionar");
 
-console.log(openModal)
+function iniciaModal(modalID) {
+    const modal = document.getElementById(modalID);
+    
+    if(modal) {
+        modal.classList.add('mostrar');
+        modal.addEventListener('click', (e) => {
+        
+        if(e.target.id == modalID) {
+            modal.classList.remove('mostrar');
+        }
+    })
+        
+        const cancelButton = document.querySelector('.btn_cancel');
+            cancelButton.addEventListener("click", () => {
+            modal.classList.remove('mostrar')
+        })
+    }
+}
 
-openModal.addEventListener("click", () => {
-    modalAdicionar.style.display = "flex";
+let openModal = document.querySelector('.btn_adicionar');
+
+openModal.addEventListener('click', () => {
+    iniciaModal('modal_adicionar');
 })
